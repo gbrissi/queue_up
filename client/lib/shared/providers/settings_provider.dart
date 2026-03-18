c96be4b5ea20377da1364cce8b5cad121875ee51
Gabriel Rissi <gbrissi123@gmail.com>
2026-03-18 11:16:55 -0300

    update

--- client/lib/shared/providers/settings_provider.dart
+++ client/lib/shared/providers/settings_provider.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:queue_up/services/shared_prefs.dart';
 
 class SettingsProvider extends ChangeNotifier {
   // Settings will include the following:
@@ -6,4 +8,16 @@ class SettingsProvider extends ChangeNotifier {
   // - Interact with Overlay Btn and Mode
   // - Orientation View Mode
   // - Sound Notifications
+  bool isInit = false;
+  late ViewMode viewMode;
+  late LogicalKeyboardKey viewBtn; // Can be set to be a key combination
+  late OrientationMode orientationMode;
+
+  Future<void> init() async {
+    isInit = true;
+    viewMode = await SharedPrefs().getViewMode() ?? ViewMode.toggle;
+    viewBtn = await SharedPrefs().getToggleViewBtn() ?? LogicalKeyboardKey.keyQ;
+    orientationMode = await SharedPrefs().getOrientationMode() ?? OrientationMode.horizontal;
+    notifyListeners();
+  }
 }
--- client/lib/windows/settings/settings.dart
+++ client/lib/windows/settings/settings.dart
@@ -1,12 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:queue_up/services/shared_prefs.dart';
+import 'package:queue_up/shared/providers/settings_provider.dart';
+import 'package:queue_up/windows/settings/components/dropdown_tile.dart';
 
 class Settings extends StatelessWidget {
   const Settings({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [],
+    return Consumer<SettingsProvider>(
+      builder: (_, provider, __) {
+        return Column(
+          children: [
+            // View Mode (Toggle/Hold)
+            // View Key
+            // Orientation
+            DropdownTile<ViewMode>(
+              icon: Icons.mode_standby,
+              text: "Select View Toggle Mode",
+              selectedItem: DropdownOption(value: provider.viewMode, item: item),
+            )
+          ],
+        );
+      }
     );
   }
 }
