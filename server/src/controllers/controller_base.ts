c70c1113ba45a79dd92b5dac80c92751cec6a6be
Gabriel Rissi <gbrissi123@gmail.com>
2026-06-03 22:53:03 -0300

    update

--- server/src/controllers/controller_base.ts
+++ server/src/controllers/controller_base.ts
@@ -5,9 +5,52 @@ interface HttpResponse<T> {
   isSuccess: boolean;
 }
 
-class ControllerBase {
-  constructor() {}
-  create() {
+class HttpError implements HttpResponse<any> {
+  statusCode: number;
+  message: string;
+  isSuccess: boolean;
 
+  constructor({
+    statusCode,
+    message,
+  }: {
+    statusCode: number;
+    message: string;
+  }) {
+    this.statusCode = statusCode;
+    this.message = message;
+    this.isSuccess = false;
+  }
+}
+
+class HttpSuccess<T> implements HttpResponse<T> {
+  data: T;
+  statusCode: number;
+  message: string;
+  isSuccess: boolean;
+
+  constructor({
+    data,
+    statusCode,
+    message,
+  }: {
+    data: T;
+    statusCode: number;
+    message: string;
+  }) {
+    this.data = data;
+    this.statusCode = statusCode;
+    this.message = message;
+    this.isSuccess = true;
+  }
+}
+
+class ControllerBase {
+  create<T>(
+    promise: Promise<T>,
+    onSuccess: (data: any) => HttpResponse<T>,
+    onError: (error: any) => HttpError,
+  ) {
+    return promise.then(onSuccess).catch(onError);
   }
 }
