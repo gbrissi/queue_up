e3b3e4380c3daba58060957bbf09ffb826ef3798
Gabriel Rissi <gbrissi123@gmail.com>
2026-06-05 23:38:59 -0300

    update

--- server/src/config/app.ts
+++ server/src/config/app.ts
@@ -1,5 +1,10 @@
 import express, { Express } from "express";
+import helmet from "helmet";
+import cors from "cors";
 
 const app: Express = express();
+// TODO:
+app.use(helmet());
+app.use(cors());
 
 export default app;
---
+++ server/src/services/image_service.ts
@@ -0,0 +1,5 @@
+class ImageService {
+  compressImage() {
+    //
+  }
+}
