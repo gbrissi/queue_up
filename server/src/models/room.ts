709168de9f3b42de90a4ad9a04fee031c9047ec7
Gabriel Rissi <gbrissi123@gmail.com>
2026-06-09 23:40:13 -0300

    update

--- server/src/models/room.ts
+++ server/src/models/room.ts
@@ -1,6 +1,51 @@
-import mongoose from "mongoose";
+import mongoose, { Schema, Document, Model } from "mongoose";
 
-interface IRoom extends Document {
+export interface IRoom extends Document {
   id: string | mongoose.Types.ObjectId;
+
+  title: string;
+  description: string;
+  image: string | mongoose.Types.ObjectId;
+
   users: string | mongoose.Types.ObjectId;
+  actions: string | mongoose.Types.ObjectId;
+
+  createdAt: Date;
+  updatedAt: Date;
 }
+
+const RoomSchema = new Schema<IRoom>(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: Schema.Types.ObjectId,
+      ref: "Image",
+      required: true,
+    },
+    users: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    actions: {
+      type: Schema.Types.ObjectId,
+      ref: "Action",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const RoomModel: Model<IRoom> = mongoose.model<IRoom>("Room", RoomSchema);
+
+export default RoomModel;
